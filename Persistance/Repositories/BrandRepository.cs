94d1576167fb3185c623a37ef36668677ac824d5
Taylan Deniz Polat <taylandenizpolat@hotmail.com>
2023-08-26 17:46:37 +0300

    brandrepo and inmemorydb added

---
+++ Persistance/Repositories/BrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Application.Services.Repositories;
+using Core.Persistance.Repositories;
+using Domain.Entities;
+using Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories;
+
+public class BrandRepository : EfRepositoryBase<Brand, Guid, BaseDbContext>, IBrandRepository
+{
+    public BrandRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
